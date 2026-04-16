7da256e432d95e7dd1bb99523486f6081b318b37
nguyenhuunhan0012 <nguyenhuunhan0012@gmail.com>
2026-04-16 14:13:33 +0700

    em nop bài

---
+++ QuanLyBanHangBuoi8_9/QuanLyBanHangBuoi8_9/QuanLyBanHang/Migrations/20260205065425_ThemCotHinhAnh.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuanLyBanHang.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThemCotHinhAnh : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HinhAnh",
+                table: "SanPham",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HinhAnh",
+                table: "SanPham");
+        }
+    }
+}
---
+++ QuanLyBanHangBuoi8_9/QuanLyBanHangBuoi8_9/QuanLyBanHang/Migrations/20260205070712_SuaTenCotLoaiSP.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuanLyBanHang.Migrations
+{
+    /// <inheritdoc />
+    public partial class SuaTenCotLoaiSP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LoaiSanXuatID",
+                table: "SanPham");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LoaiSanXuatID",
+                table: "SanPham",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
